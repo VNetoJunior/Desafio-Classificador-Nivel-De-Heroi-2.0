6d9e615c6350d72638d59d0cefa7d069f9721091
VNetoJunior <volneyjunior000@outlook.com>
2024-10-23 09:04:53 -0300

    Atualizando o Projeto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let nomeDoHeroi = "Junior"
 let xp = [" ", " ", " "]
-
+let nivelDoHeroi = " "
 let inimigos = ["Lobo", "Zumbi", "Guerreiro", "Urso", "Behemooth", "Hydra"]
 let inimigoDiaUm = inimigos[0]
 let inimigoDiaDois = inimigos[3]
@@ -15,23 +15,23 @@ switch(inimigoDiaUm) {
         break
 
     case "Zumbi":
-        xp[0] = 5000
+        xp[0] = 1500
         break
 
     case "Guerreiro":
-        xp[0] = 7000
+        xp[0] = 2000
         break
 
     case "Urso":
-        xp[0] = 12000
+        xp[0] = 3000
         break
 
     case "Behemooth":
-        xp[0] = 20000
+        xp[0] = 7000
         break
 
     case "Hydra":
-        xp[0] = 30000
+        xp[0] = 10000
         break
 
     default : console.log("Erro")
@@ -39,7 +39,7 @@ switch(inimigoDiaUm) {
 }
 
 console.log (
-    `No primeiro dia ${nomeDoHeroi} enfrentou ${inimigoDiaUm} e ganhou ${xp[0]} Pontos de XP!
+    `No Primeiro dia ${nomeDoHeroi} enfrentou ${inimigoDiaUm} e ganhou ${xp[0]} Pontos de XP!
 `)
 
 
@@ -49,23 +49,23 @@ switch(inimigoDiaDois) {
         break
 
     case "Zumbi":
-        xp[1] = 5000
+        xp[1] = 1500
         break
 
     case "Guerreiro":
-        xp[1] = 7000
+        xp[1] = 2000
         break
 
     case "Urso":
-        xp[1] = 12000
+        xp[1] = 3000
         break
 
     case "Behemooth":
-        xp[1] = 20000
+        xp[1] = 7000
         break
 
     case "Hydra":
-        xp[1] = 30000
+        xp[1] = 10000
         break
 
     default : console.log("Erro")
@@ -73,7 +73,7 @@ switch(inimigoDiaDois) {
 }
 
 console.log (
-    `No primeiro dia ${nomeDoHeroi} enfrentou ${inimigoDiaDois} e ganhou ${xp[1]} Pontos de XP!
+    `No Segundo dia ${nomeDoHeroi} enfrentou ${inimigoDiaDois} e ganhou ${xp[1]} Pontos de XP!
 `)
 
 
@@ -83,23 +83,23 @@ switch(inimigoDiaTres) {
         break
 
     case "Zumbi":   
-        xp[2] = 5000
+        xp[2] = 1500
         break
 
     case "Guerreiro":
-        xp[2] = 7000
+        xp[2] = 2000
         break
 
     case "Urso":
-        xp[2] = 12000
+        xp[2] = 3000
         break
 
     case "Behemooth":
-        xp[2] = 20000
+        xp[2] = 7000
         break
 
     case "Hydra":
-        xp[2] = 30000
+        xp[2] = 10000
         break
 
     default : console.log("Erro")
@@ -107,7 +107,7 @@ switch(inimigoDiaTres) {
 }
 
 console.log (
-    `No primeiro dia ${nomeDoHeroi} enfrentou ${inimigoDiaTres} e ganhou ${xp[2]} Pontos de XP!
+    `No Terceiro dia ${nomeDoHeroi} enfrentou ${inimigoDiaTres} e ganhou ${xp[2]} Pontos de XP!
 `)
 
 let xpTotal = soma(xp[0], xp[1], xp[2])
@@ -120,6 +120,44 @@ function soma(DiaUm, DiaDois, DiaTres) {
 }
 
 
+switch(true) {
+    
+    case (xpTotal >= 0 && xpTotal <= 1000) : 
+    nivelDoHeroi = "Ferro"
+    break
+
+    case (xpTotal >= 1001 && xpTotal <= 2000) : 
+    nivelDoHeroi = "Bronze"
+    break
+
+    case (xpTotal >= 2001 && xpTotal <= 5000) : 
+    nivelDoHeroi = "Prata"
+    break
+
+    case (xpTotal >= 5001 && xpTotal <= 7000) : 
+    nivelDoHeroi = "Ouro"
+    break
+
+    case (xpTotal >= 7001 && xpTotal <= 8000) : 
+    nivelDoHeroi = "Platina"
+    break
+
+    case (xpTotal >= 8001 && xpTotal <= 9000) : 
+    nivelDoHeroi = "Ascendente"
+    break
+
+    case (xpTotal >= 9001 && xpTotal <= 10000) : 
+    nivel = "Imortal"
+    break
+
+    case (xpTotal >= 10001) : 
+    nivelDoHeroi = "Radiante"
+    break
+
+    default: (console.log("Erro, esse nível não é válido!"))
+}
+
+
 console.log (
-    `Juntando os Três dias, ${nomeDoHeroi} conseguiu um Total de ${xpTotal}! Com isso ${nomeDoHeroi} está no *Ranking*
+    `Juntando os Três dias, ${nomeDoHeroi} conseguiu um Total de ${xpTotal}! Com isso ${nomeDoHeroi} está no Nível de ${nivelDoHeroi}!
 `)
